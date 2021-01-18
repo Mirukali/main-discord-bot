eace8b1e2023f7ce794add23ef2b228dcd5d28d2
Alikuxac <lexson270400@gmail.com>
2021-01-18 22:17:38 +0700

    Add check-update.js

---
+++ commands/botowner/check-update.js
@@ -0,0 +1,30 @@
+const Command = require('../../structures/Command');
+const exec = require('child_process').exec;
+
+module.exports = class CheckUpdateCMD extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'check-update',
+            memberName: 'check-update',
+            group: 'botowner',
+            description: 'Check update from officical github',
+            ownerOnly: true
+        })
+    }
+    run(message, args) {
+        exec(`git pull`, (error, stdout) => {
+            let response = (error || stdout);
+            console.log(response);
+            if (!error) {
+                if (response.includes("Already up to date.")) {
+                    message.channel.send('Bot already up to date. No changes since last pull')
+                } else {
+                    message.channel.send('Pulled from GitHub. Restarting bot. \n\nLogs: \n```' + response + "```")
+                    setTimeout(() => {
+                        process.exit();
+                    }, 1000)
+                };
+            }
+        });
+    }
+}
