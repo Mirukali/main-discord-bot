5447f1cee8f2eede177ccefa9ec988ef02caae0b
Alikuxac <lexson270400@gmail.com>
2021-01-19 23:35:59 +0700

    Add more command groups

--- config/command_group.js
+++ config/command_group.js
@@ -1,9 +1,11 @@
 module.exports = [
 	["admin", "Admin"],
 	["botowner", "BotOwner"],
+	["fun", "Fun"],
 	["info", "Info"],
 	["other", "Other"],
 	["owner", "Owner"],
 	["moderation", "Moderation"],
-	["voice", "Voice"]
+	["voice", "Voice"],
+	["werewolf", "Werewolf"]
 ];
