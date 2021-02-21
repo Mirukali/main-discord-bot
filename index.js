4f37936495896ff38c9eb4b7cff928b63c26ae25
Alikuxac <lexson270400@gmail.com>
2021-02-22 00:07:48 +0700

    Remove envent handler comment

--- index.js
+++ index.js
@@ -48,16 +48,10 @@ client.events = new Discord.Collection();
 fs.readdir("./events/", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
-        // If the file is not a JS file, ignore it (thanks, Apple)
         if (!file.endsWith(".js")) return;
         // Load the event file itself
         const event = require(`./events/${file}`);
-        // Get just the event name from the file name
         let eventName = file.split(".")[0];
-        // super-secret recipe to call events with all their proper arguments *after* the `client` var.
-        // without going into too many details, this means each event will be called with the client argument,
-        // followed by its "normal" arguments, like message, member, etc etc.
-        // This line is awesome by the way. Just saying'.
         client.on(eventName, event.bind(null, client));
         client.events.set(eventName, event);
         console.log(`Loaded event ${eventName}`);
