d0a1d328990d3cddeaa19f35d1504f30d4d1e7e4
Alikuxac <lexson270400@gmail.com>
2021-02-25 17:23:23 +0700

    Add utils

---
+++ util/utils.js
@@ -0,0 +1,32 @@
+module.exports = Utils = {
+    chunk: function (array, size) {
+        const chunked_arr = [];
+        for (let i = 0; i < array.length; i++) {
+            const last = chunked_arr[chunked_arr.length - 1];
+            if (!last || last.length === size) {
+                chunked_arr.push([array[i]]);
+            } else {
+                last.push(array[i]);
+            }
+        }
+        return chunked_arr;
+    },
+    titleCase: function (str, split) {
+        var splitStr = str.toLowerCase().split(split);
+        for (var i = 0; i < splitStr.length; i++) {
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        }
+        return splitStr.join(split);
+    },
+    msToHHMMSS: function (duration) {
+        let seconds = parseInt((duration / 1000) % 60),
+            minutes = parseInt((duration / (1000 * 60)) % 60),
+            hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        return hours + ":" + minutes + ":" + seconds;
+    }
+};
