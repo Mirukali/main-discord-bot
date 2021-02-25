dca378b648bb3cc772abe69a8242aafc3ccf0522
Alikuxac <lexson270400@gmail.com>
2021-02-25 17:22:59 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const exec = require('child_process').exec;
 module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`Logged in as ${client.user.id}!`);
-    client.user.setActivity(`DM's for help.`, { type: 'WATCHING' })
+    client.user.setActivity(`something for help.`, { type: 'WATCHING' })
         .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
         .catch(console.error);
 }
